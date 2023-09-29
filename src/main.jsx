e65fba8d91118e2f5fc697f8ab27e364c07fdd27
omarjafor <omarjaforchy@gmail.com>
2023-09-29 11:38:58 +0600

    some component working

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import {
 } from "react-router-dom";
 import Root from './Components/Root/Root';
 import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +18,14 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'register',
+        element: <Register></Register>
       }
     ]
   },
