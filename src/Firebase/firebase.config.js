d5f5b37c1e250b0160a975516a7dded1c25a6b02
omarjafor <omarjaforchy@gmail.com>
2023-09-29 16:08:13 +0600

    user email pass authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCYkTj5Eq_Kas_JjnaLmcY4LZUkzdH5dfw",
+    authDomain: "fir-authentication-143b8.firebaseapp.com",
+    projectId: "fir-authentication-143b8",
+    storageBucket: "fir-authentication-143b8.appspot.com",
+    messagingSenderId: "490658174511",
+    appId: "1:490658174511:web:302b36f46cff2096aad8da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
