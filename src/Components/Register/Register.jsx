a49448559bd4aafc25a9ab8b36c879da07e92e09
omarjafor <omarjaforchy@gmail.com>
2023-09-30 13:12:51 +0600

    update profiel

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from "firebase/auth";
 import app from "../../Firebase/firebase.config";
 import { useState } from "react";
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
@@ -13,10 +13,11 @@ const Register = () => {
 
     const handleRegister = e => {
         e.preventDefault();
+        const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         const accepted = e.target.checkbox.checked;
-        console.log(email, password, accepted);
+        console.log(name, email, password, accepted);
 
         if(password.length < 6){
             setRegistError('Password Should be at least 6 charecter');
@@ -36,6 +37,15 @@ const Register = () => {
         .then(result => {
             console.log(result.user);
             setSuccess('User Registration Successful')
+
+            updateProfile(result.user, {
+                displayName: name,
+                photoURL: "https://example.com/jane-q-user/profile.jpg"
+            }).then(()=>{
+                console.log('Profile Updated')
+            }).catch((err)=>{
+                console.error(err.message);
+            });
 
             sendEmailVerification(result.user)
             .then(()=> {
@@ -63,7 +73,7 @@ const Register = () => {
                         <div className="relative h-11 w-full min-w-[200px]">
                             <input
                                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                                placeholder=" " type="name"
+                                placeholder=" " type="name" name="name"
                             />
                             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                                 Name
