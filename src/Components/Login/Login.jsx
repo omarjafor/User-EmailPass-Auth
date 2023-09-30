a4fe3e608ab1bc27f5f835a090c6750124d26d05
omarjafor <omarjaforchy@gmail.com>
2023-09-30 11:39:52 +0600

    forget pass email validation

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,15 @@
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom";
 import app from "../../Firebase/firebase.config";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Login = () => {
     const auth = getAuth(app);
+    const emailRef = useRef(null);
 
     const [registError, setRegistError] = useState('');
     const [success, setSuccess] = useState('');
+    const [emailError, setEmailError] = useState('');
 
     const handleLogin = e => {
         e.preventDefault();
@@ -26,6 +28,18 @@ const Login = () => {
             setRegistError(error.message);
         })
     }
+
+    const handleForgetPassword = () => {
+        const email = emailRef.current.value;
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        setEmailError('');
+
+        if(!email){
+           setEmailError('First give your email');
+        }else if(!emailPattern.test(email)){
+            setEmailError('Please write a valid email address');
+        }
+    }
     return (
         <div className="flex justify-center border-red border border-solid h-[90vh] items-center">
             <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
@@ -39,7 +53,7 @@ const Login = () => {
                         <div className="relative h-11 w-full min-w-[200px]">
                             <input
                                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                                placeholder=" " name="email" type="email"
+                                placeholder=" " name="email" type="email" ref={emailRef}
                             />
                             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                                 Email
@@ -55,8 +69,8 @@ const Login = () => {
                             </label>
                         </div>
                         <div className="-ml-2.5">
-                            <div className="text-left ml-6">
-                                <p>Forget Password?</p>
+                            <div className="text-left ml-6 cursor-pointer">
+                                <a onClick={handleForgetPassword}>Forget Password?</a>
                             </div>
                         </div>
                     </div>
@@ -73,6 +87,9 @@ const Login = () => {
                         }
                         {
                             success && <p className="text-green-600"> {success} </p>
+                        }
+                        {
+                            emailError && <p className="text-red-600"> {emailError} </p>
                         }
                         <p className="mt-6 flex justify-center font-sans text-sm font-light leading-normal text-inherit antialiased">
                             Don&apos;t have an account?
